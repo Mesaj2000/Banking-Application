50db4f9fc357188a48daad2109a3fcd1212eddf2
= <=>
2020-09-22 20:06:18 -0400

    just doing some more github integration tests

--- test.py
+++ test.py
@@ -1,11 +1,23 @@
-x = 5
-y = 2
-z = x + y * 2
-print("this is a test for when a line is really" " really really really really"
-      " really long omg pepega loooolsz")
+class Animal(object):
+    name = "[default name]"
+    sound = "[default sound]"
+
+    def declare_name(self):
+        print("I am a {}".format(self.name))
+
+    def make_sound(self):
+        print("My sound is {}".format(self.sound))
+
+    def format_tester(self):
+        print("{0}! I am a {1} and because I am a {1} I like to go {0}! {0}!"
+              .format(self.sound, self.name))
 
 
-print(x+y * z)
-
-# comment
-print("memes")
+x = Animal()
+x.declare_name()
+x.make_sound()
+x.name = "duck"
+x.sound = "quack"
+x.declare_name()
+x.make_sound()
+x.format_tester()
